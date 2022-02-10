10f98aedbf5880f449dcd10f72725f2cc2598d0a
Ade <shennaike2386@hotmail.com>
2022-02-10 16:05:56 -0500

    finished test 3 and 4

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {render, screen, waitFor } from '@testing-library/react';
+import {fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ContactForm from './ContactForm';
 
+let firstName, lastName, email, message, submitBtn, error
+
 beforeEach(() => {
     render (<ContactForm/>)
+     firstName = screen.getByLabelText('First Name', {exact: false})
+     lastName = screen.getByLabelText('last Name', {exact: false})
+     email = screen.getByLabelText('email', {exact: false})
+     message = screen.getByLabelText('message', {exact: false})
+     submitBtn = screen.getByRole('button')
+
+    //  error = screen.getAllByText(/error/i)
 });
 
 afterEach(() => {
@@ -26,11 +35,22 @@ test('renders the contact form header', ()=> {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    userEvent.type(firstName, 'ade')
+
+    const error = screen.getAllByTestId("error")
+
+    expect(error).toHaveLength(1)
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    userEvent.type(firstName, "")
+    userEvent.type(lastName, "")
+    userEvent.type(email, "")
+    userEvent.click(submitBtn)
+
+    const error = screen.getAllByTestId("error")
+
+    expect(error).toHaveLength(3)
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
